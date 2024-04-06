79a27320e54c47d7ab66285fb3747edf81572628
atk <akirschenman@gmail.com>
2024-04-06 17:49:20 -0500

    Initialize react.devicelist

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DeviceList from "./components/DeviceList/DeviceList"
+
+function App() {
+  return (
+    <div>
+      <DeviceList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DeviceList/DeviceList.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+export default function VerticalTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: 'divider' }}
+    >
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+        <Tab label="Item Four" {...a11yProps(3)} />
+        <Tab label="Item Five" {...a11yProps(4)} />
+        <Tab label="Item Six" {...a11yProps(5)} />
+        <Tab label="Item Seven" {...a11yProps(6)} />
+      </Tabs>
+  );
+}
